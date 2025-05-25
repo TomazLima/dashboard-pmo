e27fff4bd58b32cd5735bfbf1ea0433af49c0f23
TomazLima <tomaz.delima@icloud.com>
2025-05-25 00:21:58 -0300

    Remove password hash generator

--- gerar_hash.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-def make_hashes(password):
-    return hashlib.sha256(str.encode(password)).hexdigest()
-
-# Substitua pela sua senha desejada
-senha = "minha_senha_secreta_123"
-hash_gerado = make_hashes(senha)
-print(f"Hash da senha '{senha}': {hash_gerado}")
